e68a69c7eab75d9d717b76307d51c8e82dca29a0
skb461 <53707185+skb461@users.noreply.github.com>
2022-02-25 15:42:54 +0600

    Add files via upload

---
+++ bundle5.js
@@ -0,0 +1,93 @@
+(function (d3) {
+  'use strict';
+
+  const titleText = 'Pricing Based on Number of Bedrooms and Baths';
+  const xAxisLabelText = '';
+
+  const svg = d3.select('#e');
+
+  const width = +svg.attr('width');
+  const height = +svg.attr('height');
+
+  const render = data => {
+    const xValue = d => d['population'];
+    const yValue = d => d.country;
+    const margin = { top: 100, right: 40, bottom: 100, left: 50};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+    
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, xValue)])
+      .range([0, innerWidth]);
+    
+    const yScale = d3.scaleBand()
+      .domain(data.map(yValue))
+      .range([0, innerHeight])
+      .padding(0.1);
+    
+    const g = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+    
+    const xAxisTickFormat = number =>
+      d3.format('.3s')(number)
+        .replace('G', 'B');
+    
+    const xAxis = d3.axisBottom(xScale)
+      .tickFormat(xAxisTickFormat)
+      .tickSize(-innerHeight);
+    
+    g.append('g')
+      .call(d3.axisLeft(yScale))
+      .selectAll('.domain, .tick line')
+        .remove();
+    
+    const xAxisG = g.append('g').call(xAxis)
+      .attr('transform', `translate(0,${innerHeight})`);
+    
+    xAxisG.select('.domain').remove();
+    
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 65)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'red')
+        .text(xAxisLabelText);
+    
+    g.selectAll('rect').data(data)
+      .enter().append('rect')
+        .attr('y', d => yScale(yValue(d)))
+        .attr('width', d => xScale(xValue(d)))
+        .attr('height', yScale.bandwidth());
+    
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y', -10)
+        .text(titleText);
+  };
+
+  d3.csv('data5.csv').then(data => {
+    data.forEach(d => {
+      d.population = +d.population;
+    });
+    render(data);
+  });
+
+}(d3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
